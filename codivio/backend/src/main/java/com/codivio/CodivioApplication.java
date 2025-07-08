1612b663e07ef35ca18f74148879068e88dff7fe
Xiu Lin <lxin233@163.com>
2025-07-08 18:05:19 +0800

    启动类

---
+++ codivio/backend/src/main/java/com/codivio/CodivioApplication.java
@@ -0,0 +1,11 @@
+package com.codivio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodivioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodivioApplication.class, args);
+    }
+}
